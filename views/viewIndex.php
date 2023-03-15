8ff7b0d3c9d20555e908d29ea3fb1a13efa7090c
Dorian <dorian.perrier@epitech.eu>
2023-03-15 12:11:28 +0100

    [+] Now when simple rt, the likes, comms etc.. from the origin rt are correctly displayed

--- views/viewIndex.php
+++ views/viewIndex.php
@@ -44,6 +44,46 @@ $tweet = new TweetManager;
                             <img src="https://via.placeholder.com/150 " alt="tweet content" class="h-[504px] w-[504px] rounded-xl mt-2">
                         </p>
                     </div>
+                    <div class="m-4 border flex gap-8">
+                    <div class="flex cursor-pointer hover:text-blue-400 relative">
+                        <div class="retweet">
+                            <button value="<?= $data->origin() ?>" name="retweetButton" class=" flex retweetButton" >
+                                <i class="fa-solid fa-retweet mt-1"></i>
+                                <p class="ml-1"><?= $tweet->retweetsNumber($data->origin()) ?></p>
+                            </button>
+                            <div class="retweetOverlay hidden absolute bg-gray-200 rounded-xl z-10">
+                                <div class="flex cursor-pointer hover:bg-gray-100">
+                                    <button value="<?= $data->origin() ?>" name="retweetButton" type="button" class="retweetButton">
+                                        <i class="fa-solid fa-pen"></i>
+                                        Retweeter
+                                    </button>
+                                </div>
+                                <div class="flex cursor-pointer hover:bg-gray-100">
+                                    <button value="<?= $data->origin() ?>" name="quoteTweetButton" type="button" class="quoteTweetButton">
+                                        <i class="fa-solid fa-pen"></i>
+                                        Citer le Tweet
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                        <div class="flex cursor-pointer hover:text-green-400">
+                            <button value="<?= $data->origin() ?>" name="commentButton" type="button" class="commentButton">
+                                <i class="fa-regular fa-comment mt-1"></i>
+                            </button>
+                            <p class="ml-1"><?= $tweet->commentsNumber($data->origin()) ?></p>
+                        </div>
+                        <div class="<?= $tweet->isLiked($data->origin()) ? "flex cursor-pointer hover:text-pink-500 text-pink-400" : "flex cursor-pointer hover:text-pink-400"?>">
+                            <button value= <?= implode("-", ["tweet_id" => $data->origin(), "user_id" => $data->user_id()]) ?> name="likeButton" type="button" class="likeButton">
+                                <i class= "<?= $tweet->isLiked($data->origin()) ? "fa-heart mt-1 fa-solid" : "fa-heart mt-1 fa-regular" ?>"></i>
+                            </button>
+                            <p class="ml-1"><?= $tweet->likesNumber($data->origin()) ?></p>
+                        </div>
+                        <form action="tweet" method="post" class="tweetMainForm">
+                            <button class="seeComments" style="cursor: pointer;" value="<?= $data->id() ?>" name="seeComments" type="submit">Voir plus</button>
+                        </form>
+                    </div>
+                </div>
                 <?php } else { ?>
                 <div class="flex p-4">
                     <img src="https://via.placeholder.com/150" alt="avatar" class="w-12 h-12 rounded-full">
@@ -78,7 +118,6 @@ $tweet = new TweetManager;
                         </div>
                     </div>
                 <?php endif ?>
-                <?php } ?>
                 <div class="m-4 border flex gap-8">
                     <div class="flex cursor-pointer hover:text-blue-400 relative">
                         <div class="retweet">
@@ -119,6 +158,7 @@ $tweet = new TweetManager;
                         </form>
                 </div>
             </div>
+            <?php } ?>
         <?php endforeach ?>
     <?php endif ?>
 </div>
